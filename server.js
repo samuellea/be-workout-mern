c53dff38a111fb1ea2b1d7b943c27a392008e2ff
samuellea <samuel.lea@live.co.uk>
2025-04-11 13:55:39 +0100

    Added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
+const cors = require('cors'); // Importing CORS
 
 const workoutRoutes = require('./routes/workouts');
 
@@ -12,10 +13,16 @@ const app = express();
 // 📦 middleware
 app.use(express.json()); // this will parse any bodies coming in on requests as JSON, and attaches that on the request object as req.body, so we can access it in the request handlers on our routes
 
+// Use CORS to allow requests from your frontend
+app.use(
+  cors({
+    origin: '*', // Allow all origins (for debugging purposes)
+  })
+);
+
 app.use((req, res, next) => {
   // this will fire for every request that comes in to our express app
   console.log(req.path, req.method);
-  // without calling next when we're finished with our middleware, we never advance to app.get, or app.post etc.
   next();
 });
 
